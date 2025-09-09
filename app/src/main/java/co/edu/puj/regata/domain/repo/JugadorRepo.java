75b060428665b9b314ee446155231a4757d584ae
Santiago Avila <avilabarbudosantiago@gmail.com>
2025-09-09 13:33:35 -0500

    Corrección app directory

--- app/src/main/java/co/edu/puj/regata/domain/repo/JugadorRepo.java
+++
@@ -1,9 +0,0 @@
-package co.edu.puj.regata.domain.repo;
-
-import co.edu.puj.regata.domain.entity.Jugador;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface JugadorRepo extends JpaRepository<Jugador, Long> {
-    Optional<Jugador> findByNombre(String nombre);
-}
--- app/src/main/java/co/edu/puj/regata/domain/repo/MapaRepo.java
+++
@@ -1,8 +0,0 @@
-package co.edu.puj.regata.domain.repo;
-
-import co.edu.puj.regata.domain.entity.Mapa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MapaRepo extends JpaRepository<Mapa, Long> {
-    // Consultas personalizadas si son necesarias
-}
